b4ed193caab81a4b40b197dfc1ae158d2c71c1ad
Ioanna Papaioannou <ipapioani@gmail.com>
2023-11-07 23:30:05 +0100

    commit after creating NavLinks and Route paths in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import {Routes, Route, Link, NavLink} from "react-router-dom"
 import StillPlanning from './components /StillPlanning'
+import RoadTrips from './components /RoadTrips'
+import InteractiveMap from './components /InteractiveMap'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
+        <h1>Relax and roaδ it</h1>
+        <NavLink to="/" activeStyle="active">StillPlanning</NavLink>
+        <br/>
+        <NavLink to="/trips" activeStyle="active">RoadTrips</NavLink>
+
        <Routes>
         <Route path = "/" element= {<StillPlanning />} />
+        <Route path = "/trips" element= {<RoadTrips />} />
+        <Route path = "/map" element= {<InteractiveMap />} />
        </Routes>
       </div>
   )
---
+++ client/src/components /RoadTrips.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function RoadTrips() {
+    return (
+      <div>Routes you´ll love...</div>
+    )
+  }
