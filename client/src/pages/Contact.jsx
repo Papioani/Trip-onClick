29ab1a47e97e0f1633b4cfae9a02246a792c6047
Ioanna Papaioannou <ipapioani@gmail.com>
2023-11-13 00:20:12 +0100

    commit of contact form code

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,16 +1,20 @@
 import React from "react"
-import { useNavigate } from "react-router-dom";
+
 
 export default function Contact() {
-  let navigate = useNavigate();
-
-  const handleClick =() => {
-    navigate("/contact")
-  }
-
+  
     return (
-      <div style={{color: "green"}} >
-      <button onClick={handleClick}>Contact</button>
-      </div>
-    )
-  }
+      <form  className='ContactForm'>
+         <input type='text' name='name' placeholder='Name'></input>
+         <input type='email' name='email' placeholder='Email address'></input>
+         <input type='text' name='comments' placeholder='How can we help?'></input>
+         <textarea  rows={5} name='message' placeholder='Message' ></textarea>
+         <button className='submit-btn' type='submit'>
+            Submit
+         </button>
+        </form>              
+                               
+             
+    );
+  };
+  
