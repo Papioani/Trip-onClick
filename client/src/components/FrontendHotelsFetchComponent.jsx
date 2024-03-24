3b83904414cb8df0f785929f79b193f194c84217
Ioanna Papaioannou <ipapioani@gmail.com>
2024-03-24 13:12:45 +0100

    changing the API for hotel fecthing to tripadvisor16 and adjusting the code

--- client/src/components/FrontendHotelsFetchComponent.jsx
+++ client/src/components/FrontendHotelsFetchComponent.jsx
@@ -3,27 +3,80 @@ import SpainMapComponent from "./SpainMapComponent";
 import ManiHotelFavourites from "./ManiHotelFavourites";
 
 function FrontendHotelsFetchComponent() {
-  // create a form to store the necessary parameters for the hotel search
+  // create a form to store the parameters for the hotel search
   const EMPTY_FORM = {
     CheckIn: "",
     CheckOut: "",
     Adults: "",
     Rooms: "",
-    Rating: "4",
-    PriceMin: "100",
-    PriceMax: "200",
   };
   // a state variable to store the parameters
-  const [flightParameters, setFlightParameters] = useState(EMPTY_FORM);
-  // a variable for the hotels
+  const [hotelParameters, setHotelParameters] = useState(EMPTY_FORM);
+  // // a state variable for the destinations
+  const [destination, setDestination] = useState("");
+  // a variable for the results
   const [results, setResults] = useState([]);
   // a state variable for the errors
   const [error, setError] = useState("");
   // a state variable for the loading
   const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    // Without useEffect, this code would be executed every time the component re-renders.
+
+  const handleClick = async (click) => {
+    setIsLoading(true);
+    setHotelParameters(EMPTY_FORM);
+    setDestination("");
+    setResults([]);
+    setError("");
+    const url = `https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchLocation?query=${geoId}`;
     const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "314a76c0f8mshba931602e18bfcfp1f7807jsn79a0981b285a",
+        "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
+      },
+    };
+
+    try {
+      // Wait for both fetch operations to complete
+      const [searchedAirportCodeOrigin, searchedAirportCodeDestination] =
+        await Promise.all([
+          searchAirport(originUrl, "cityOrigin", options),
+          searchAirport(destinationUrl, "cityDestination", options),
+        ]);
+      console.log(options);
+      console.log("Origin Airport Code:", searchedAirportCodeOrigin);
+      console.log("Destination Airport Code:", searchedAirportCodeDestination);
+      /// !!!!!!!!!!!!!!!!!!!!!!
+      const ticketUrl = `https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights?sourceAirportCode=${searchedAirportCodeOrigin}&destinationAirportCode=${searchedAirportCodeDestination}&date=${
+        flightParameters.outboundDate
+      }&itineraryType=${
+        flightParameters.itineraryType
+      }&sortOrder=DURATION&numAdults=${
+        flightParameters.numAdults
+      }&numSeniors=0&classOfService=ECONOMY${
+        flightParameters.itineraryType === "ROUND_TRIP"
+          ? `&returnDate=${flightParameters.returnDate}`
+          : ""
+      }&pageNumber=1&currencyCode=USD`; ///   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      // Perform the new fetch using the obtained airport codes
+      console.log(ticketUrl);
+      const ticketResponse = await fetch(ticketUrl, options);
+      if (!ticketResponse.ok) {
+        throw new Error("Failed to fetch flight search data");
+      }
+      const result = await ticketResponse.json();
+      setResults(result);
+      setIsLoading(true);
+      console.log(result);
+    } catch (error) {
+      setIsLoading(false);
+      setError(`Something went wrong: ${error.message}`);
+      console.error(error);
+    }
+  };
+  // useEffect(() => {
+  // Without useEffect, this code would be executed every time the component re-renders.
+  /* const options = {
       method: "GET",
       headers: {
         "X-RapidAPI-Key": "693a320c7amshf8a3f0479327cbap12dca4jsn254f4f98016a",
@@ -51,7 +104,7 @@ function FrontendHotelsFetchComponent() {
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
+  }, []); */
 
   return (
     <div>
