7d631bf8d0799fd15b8aea904937e3fab1ca9e57
Ioanna Papaioannou <ipapioani@gmail.com>
2024-05-16 16:47:02 +0200

    applying tailwind style to the parameters buttons

--- client/src/components/TripParametersComponent.jsx
+++ client/src/components/TripParametersComponent.jsx
@@ -104,6 +104,8 @@ export default function TripParametersComponent(props) {
       <br />
       <div className="adults-container">
         <Button
+          type="button"
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           variant="contained"
           style={{ backgroundColor: "#2eb872", color: "white" }}
           onClick={adultHandleClick}
@@ -116,6 +118,7 @@ export default function TripParametersComponent(props) {
       </div>
       <div className="rooms-container">
         <Button
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           variant="contained"
           style={{ backgroundColor: "#2eb872", color: "white" }}
           onClick={roomHandleClick}
