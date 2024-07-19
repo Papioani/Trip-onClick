2cd9a80477659b6aee70f1f9ef9577d303b3bddd
Ioanna Papaioannou <ipapioani@gmail.com>
2024-07-19 21:07:35 +0200

    adjusting routes/places endpoint

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');  //  require, not import for backend 
-var apiRouter = require('./routes/places');   /* importing it !!!!!!!!!!!!!!!!!!! this is actually the api.js */
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors"); //  require, not import for backend
+var apiRouter = require("./server/routes/places"); /* importing it !!!!!!!!!!!!!!!!!!! this is actually the api.js */
 
 var app = express();
-app.use(cors()); // cors adds some headers to the my response 
-app.use(logger('dev'));
+app.use(cors()); // cors adds some headers to the my response
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public'))); /* if we want to put the client inside of the public folder , we´d keep it  */
 // so that "/" will be ... /api
-app.use('/api/places', apiRouter);  
-
+app.use("/api/places", apiRouter);
 
 module.exports = app;
