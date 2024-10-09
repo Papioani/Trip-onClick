836bd8e267b10758cc3093e2140fa461035ec344
Ioanna Papaioannou <ipapioani@gmail.com>
2024-10-09 22:10:51 +0200

    adding routers in app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors"); //  require, not import for backend
-var apiRouter = require("./server/routes/places"); /* importing it !!!!!!!!!!!!!!!!!!! this is actually the api.js */
+const tripsRouter = require("./routes/trips"); /* importing it !!!!!!!!!!!!!!!!!!! this is actually the api.js */
+const locationsRouter = require("./routes/locations");
+const tripsRoutesRouter = require("./routes/tripsRoutes");
+const usersRouter = require("./routes/users");
 
 var app = express();
 app.use(cors()); // cors adds some headers to the my response
@@ -13,6 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public'))); /* if we want to put the client inside of the public folder , we´d keep it  */
 // so that "/" will be ... /api
-app.use("/api/places", apiRouter);
+app.use("/api/trips", tripsRouter);
+app.use("/api/locations", locationsRouter);
+app.use("/api/tripsRoutes", tripsRoutesRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
