5d4e193e692156279292d55e057b7a6e71c57367
Ioanna Papaioannou <ipapioani@gmail.com>
2024-10-09 22:16:05 +0200

    formatting app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors"); //  require, not import for backend
+
 const tripsRouter = require("./routes/trips"); /* importing it !!!!!!!!!!!!!!!!!!! this is actually the api.js */
 const locationsRouter = require("./routes/locations");
-const tripsRoutesRouter = require("./routes/tripsRoutes");
+const roadTripRoutesRouter = require("./routes/roadTripRoutes");
 const usersRouter = require("./routes/users");
 
 const app = express();
@@ -15,10 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public'))); /* if we want to put the client inside of the public folder , we´d keep it  */
+
 // so that "/" will be ... /api
 app.use("/api/trips", tripsRouter);
 app.use("/api/locations", locationsRouter);
-app.use("/api/tripsRoutes", tripsRoutesRouter);
+app.use("/api/roadTripRoutes", roadTripRoutesRouter);
 app.use("/api/users", usersRouter);
 
 module.exports = app;
